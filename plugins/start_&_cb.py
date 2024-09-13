7ad1001655e63cb1aa7295f54499eac1edadc7ae
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-13 18:21:35 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -1,9 +1,9 @@
 import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery, Message, InputMediaPhoto
-
+from pyrogram.errors import *
 from helper.database import madflixbotz
-from config import Config, Txt  
+from config import AUTH_CHANNEL, Config, Txt  
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start(client, message):
