1f3cdc31c77424af22bf729f51a76e75c357d394
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-13 20:48:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001984768732") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     
     # wes response configuration     
